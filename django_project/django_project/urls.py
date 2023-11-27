f02ade16a8cbcea8d56e0dbd6c3ba6b9f976335d
p4n.in <143927285+codeswithpankaj@users.noreply.github.com>
2023-11-27 09:59:35 +0000

    update codes

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -22,7 +22,7 @@ from django.views.generic.base import TemplateView  # new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("log/", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
 ]
 
 
